0ce138567ea5f6057d4b4e6bf597baeef25f2e4c
lean <leanmaci810@gmail.com>
2026-05-01 13:28:55 -0300

    feat: add onboarding and allow-location routes with wildcard redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: "**",
+    redirectTo: "allow-location"
+  },
+  {
+    path: 'onboarding',
     loadComponent: () => import('./pages/onboarding/onboarding.page').then(m => m.OnboardingPage)
+  },
+  {
+    path: 'allow-location',
+    loadComponent: () => import('./pages/allow-location/allow-location.page').then(m => m.AllowLocationPage)
   }
 ];
